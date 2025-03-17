5a0f5f31273243ece35cddbb1038547811ed6b36
toto-0808 <whita@outlook.jp>
2025-03-18 00:25:26 +0900

    r3fのサンプル

--- src/app/(main)/three/tutorial/page.tsx
+++ src/app/(main)/three/tutorial/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+'use client';
 
-const tutorial = () => {
+import * as THREE from 'three'
+import React, { JSX, useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props: JSX.IntrinsicElements['mesh']){
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
   return (
-    <div>
-      tutorial
-    </div>
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'blue'} />
+    </mesh>
   )
 }
 
-export default tutorial
+export default function tutorial() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+  </Canvas>
+  )
+}
